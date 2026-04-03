adb9eb7d9828b262e8695b6ed8e22b1998c51824
Raahim Irfan <work.raahim@gmail.com>
2026-04-03 18:58:48 +0500

    fix: bind server to 0.0.0.0 for Railway compatibility

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { app } = require("./src/app");
 const { env } = require("./src/config/env");
 
-app.listen(env.PORT, () => {
-    console.log(`API listening on http://localhost:${env.PORT}`);
+app.listen(env.PORT, "0.0.0.0", () => {
+    console.log(`API listening on http://0.0.0.0:${env.PORT}`);
 });
 
